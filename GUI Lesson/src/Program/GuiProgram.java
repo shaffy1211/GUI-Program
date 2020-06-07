6add9781cef6213290ea104c72bca426cf40a2d9
Safkat Khan <joga90zim@gmail.com>
2020-06-07 15:59:13 +0800

    Start of GUI

---
+++ GUI Lesson/src/Program/GuiProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Program;
+
+/**
+ * @author Safkat Khan
+ *
+ */
+public class GuiProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
