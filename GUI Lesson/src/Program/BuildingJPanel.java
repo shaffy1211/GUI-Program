ca018e3481c2e6b5a0291f7e5f2771e5cefcb05f
Safkat Khan <joga90zim@gmail.com>
2020-06-19 04:07:21 +0800

    GUI updates

--- GUI Lesson/src/Program/BuildingJPanel.java
+++ GUI Lesson/src/Program/BuildingJPanel.java
@@ -1,5 +1,16 @@
 package Program;
 
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
 public class BuildingJPanel {
-
+//JLabel: Displays uneditable text or icons
+	//Jtextfield: recieves input from the user
+	//Jbutton: triggers an event when clicked with the mouse
+	//JCheckbox: Specifies an option that can be selected or not selected
+	//JComboBox: A drop down List of items which user can make a selection
+	//Jlist: A list of items from which the user can make a selection by clicking on any one of them
+	//JPanel: the whole area all the above components can be placed in
+	
+	
+	
 }
